ea8d94c5f87c5ba8879d3d8977ca700d8b091e8c
Alvin <sanghup123@naver.com>
2019-11-05 19:30:33 +0900

    setting store

--- src/store/configure.js
+++ src/store/configure.js
@@ -1,24 +1,24 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 
-import reducers from './modules';
+import reducers from './reducers';
 import rootSaga from './sagas';
 
 // saga 미들웨어를 생성
 const sagaMiddleware = createSagaMiddleware();
 const middleWares = [sagaMiddleware];
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const isDevelopment  = process.env.NODE_ENV === 'development';
 
 // 개발환경일때만 크롬 확장프로그램 추가
-const enhancers =
-  process.env.NODE_ENV === 'development'
-    ? composeEnhancers(applyMiddleware(...middleWares))
-    : applyMiddleware(...middleWares);
+const devTools = isDevelopment && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+const composeEnhancers = devTools || compose;
 
 const configure = initialState => {
-  const store = createStore(reducers, initialState, enhancers);
+  const store = createStore(reducers, initialState, composeEnhancers(applyMiddleware(...middleWares)));
+
   sagaMiddleware.run(rootSaga);
+
   return store;
 };
 
