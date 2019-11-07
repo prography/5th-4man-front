10fd45e1037d6134c123ae599516c93a368de551
sanghup1234 <sanghup123@naver.com>
2019-11-07 22:30:32 +0900

    Router setting 🔫

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import App from './components/App';
+import Root from './Root';
+import store from './store';
 import * as serviceWorker from './serviceWorker';
 
-import store from './store';
+import 'styles/main.scss';
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Root />
   </Provider>,
   document.getElementById('root'),
 );
