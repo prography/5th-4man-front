3186be265a7efc07c24afb0ff24a36ed99bb1851
Alvin <sanghup123@naver.com>
2019-11-11 18:08:47 +0900

    홈 타이틀 추가

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,6 +9,11 @@ const Home = () => {
       <Banner />
       <section id="content">
         <div className="container">
+          <div className="display-flex flex-direction-column align-items-center content-title-aria">
+            <h2 className="text-bold">카테고리별 인기 팀</h2>
+            <p className="text-light">각각 카테고리별로 인기있는 팀들을 모아놨어요!</p>
+            <div className="bar" />
+          </div>
           <ListContainer />
         </div>
       </section>
