e35caf823652d37cca11d1356e6e6b4d3d555ed7
Alvin <sanghup123@naver.com>
2019-11-11 18:08:04 +0900

    배너 수정 🖼

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,7 +4,15 @@ const Banner = () => {
   return (
     <section id="banner">
       <div className="bannerBg">
-        <h2 className="text-bold">함께 개발할 사람, 3초만에 알아보세요</h2>
+        <div className="container display-flex">
+          <div className="stack-choice-area">
+            여기에 개발 스택을 고를 수 있게 개발 예정
+          </div>
+          <div className="banner-title">
+            <h2 className="text-bold">함께 개발할 사람,<br /> 3초만에 알아보세요</h2>
+            <p>같이 개발할 사람 찾기가 어려웠다면<br />개같하로 나와 FIT이 맞는 사람을 찾아보세요!</p>
+          </div>
+        </div>
       </div>
     </section>
   );
