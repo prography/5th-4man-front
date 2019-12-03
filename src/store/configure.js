e9a8094bcc69cb447595e248f0df63ddee73506d
kokihoon <rhrlgns777@gmail.com>
2019-12-04 00:01:44 +0900

    add localstorage

--- src/store/configure.js
+++ src/store/configure.js
@@ -4,6 +4,26 @@ import createSagaMiddleware from 'redux-saga';
 import reducers from './reducers';
 import rootSaga from './sagas';
 
+const saveToLcalStorage = state => {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem('state', serializedState);
+  } catch (e) {
+    Error(e);
+  }
+};
+
+const localFromLocalStorage = () => {
+  try {
+    const serializedState = localStorage.getItem('state');
+    if (serializedState === null) return undefined;
+    return JSON.parse(serializedState);
+  } catch (e) {
+    Error(e);
+    return undefined;
+  }
+};
+
 // saga 미들웨어를 생성
 const sagaMiddleware = createSagaMiddleware();
 const middleWares = [sagaMiddleware];
@@ -14,14 +34,16 @@ const isDevelopment = process.env.NODE_ENV === 'development';
 const devTools = isDevelopment && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
 const composeEnhancers = devTools || compose;
 
-const configure = initialState => {
+const configure = () => {
+  const persistedState = localFromLocalStorage();
   const store = createStore(
     reducers,
-    initialState,
+    persistedState,
     composeEnhancers(applyMiddleware(...middleWares)),
   );
 
   sagaMiddleware.run(rootSaga);
+  store.subscribe(() => saveToLcalStorage(store.getState()));
 
   return store;
 };
