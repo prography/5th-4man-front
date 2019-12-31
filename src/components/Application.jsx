6b2c647c44de0a541b7ec42d8770e0f7733aff72
Alvin <sanghup123@naver.com>
2019-12-31 12:49:01 +0900

    form validation 추가

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -11,30 +11,32 @@ const Application = ({ form }) => {
     setReason(e.target.value);
   };
 
+  const handleSubmit = e => {
+    e.preventDefault();
+    form.validateFields((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
+      }
+    });
+  }
+
   return (
     <div className="modal-body application-modal">
-      <Form className="application-form">
+      <Form className="application-form" onSubmit={handleSubmit}>
         <div className="mb-20">
           <Form.Item>
             {getFieldDecorator('reason', {
               rules: [
-                { required: true, message: 'Please input your username!' },
+                { required: true, message: '지원동기는 필수로 입력해야 합니다!' },
               ],
             })(
-              // <TextArea
-              //   placeholder="팀장이 회원님에 대해 알 수 있도록 자기소개와 지원동기를 작성해주세요."
-              //   allowClear
-              //   onChange={onChange}
-              //   value={reason}
-              //   autoSize={{ minRows: 6 }}
-              // />,
-              <Input
-                placeholder="깃허브 아이디를 작성해주세요."
-                prefix={
-                  <Icon type="github" style={{ color: 'rgba(0,0,0,.25)' }} />
-                }
-                size="large"
-              />,
+              <TextArea
+                placeholder="팀장이 회원님에 대해 알 수 있도록 자기소개와 지원동기를 작성해주세요."
+                allowClear
+                onChange={onChange}
+                value={reason}
+                autoSize={{ minRows: 6 }}
+              />
             )}
           </Form.Item>
         </div>
