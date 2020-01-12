f37e7d3bd5d0d1d742027dcd224df956828282ad
kokihoon <rhrlgns777@gmail.com>
2020-01-13 02:03:27 +0900

    error upload 1 data, tag search

--- src/containers/TeamCreateContainer.jsx
+++ src/containers/TeamCreateContainer.jsx
@@ -8,10 +8,13 @@ import {
   DatePicker,
   Upload,
   Modal,
+  Tag,
+  AutoComplete,
+  Divider,
 } from 'antd';
 import { withRouter } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import * as PostAPI from 'lib/api/post';
+import * as api from 'lib/api/post';
 import swal from 'sweetalert';
 
 const getBase64 = file => {
@@ -26,10 +29,66 @@ const getBase64 = file => {
 const TeamCreateContainer = props => {
   const [previewVisible, setPreviewvisible] = useState(false);
   const [previewImage, setPreviewimage] = useState('');
-  const [image, setImage] = useState('');
+  const [image, setImage] = useState(null);
+  const [selectTags, setSelectTags] = useState([]);
+  const [searchTags, setSearchTags] = useState([]);
+  const [input, setInput] = useState('');
+
   const { access } = useSelector(state => state.user);
   const { getFieldDecorator } = props.form;
   const { TextArea } = Input;
+
+  const addItem = async () => {
+    const params = {
+      name: input,
+    };
+    await api.insertTag(params);
+
+    if (searchTags.length < 6) {
+      setSearchTags(
+        [...searchTags, input].filter(
+          (item, index) => [...searchTags, input].indexOf(item) === index,
+        ),
+      );
+
+      setInput('');
+    }
+  };
+
+  const handleTagSearch = async value => {
+    setInput(value);
+    try {
+      if (value !== '') {
+        const getTag = await api.getTagData(value);
+
+        if (getTag.data.length === 0) {
+          setSelectTags([]);
+        } else {
+          let selectTagList = [];
+          for (let i = 0; i < getTag.data.length; i += 1) {
+            selectTagList = [...selectTags, getTag.data[i].name].filter(
+              (item, index) =>
+                [...selectTags, getTag.data[i].name].indexOf(item) === index,
+            );
+          }
+
+          setSelectTags(selectTagList);
+        }
+      }
+    } catch (err) {}
+  };
+
+  const onSelect = selectedItems => {
+    setSearchTags(
+      [...searchTags, selectedItems].filter(
+        (item, index) => [...searchTags, selectedItems].indexOf(item) === index,
+      ),
+    );
+  };
+
+  const handleClose = removedTag => {
+    setSearchTags(searchTags.filter(tag => tag !== removedTag));
+  };
 
   const handleSubmit = e => {
     e.preventDefault();
@@ -46,9 +105,8 @@ const TeamCreateContainer = props => {
         );
         formdata.append('access', access);
         formdata.append('image', image);
-
-        const data = await PostAPI.createTeam(formdata);
-
+        formdata.append('tags', searchTags);
+        const data = await api.createTeam(formdata);
         swal('팀생성 완료!', 'success').then(() => {
           window.location.href = `/team/${data.data.id}`;
         });
@@ -72,9 +130,14 @@ const TeamCreateContainer = props => {
     setPreviewvisible(true);
   };
 
-  const handleChange = ({ file, fileList }) => {
+  const handleChange = ({ file }) => {
     setImage(file.originFileObj);
+
+    if (file.status === 'removed') {
+      setImage(null);
+    }
   };
+
   const buttonItemLayout = {
     wrapperCol: { span: 28 },
   };
@@ -149,14 +212,80 @@ const TeamCreateContainer = props => {
                     ],
                   })(<DatePicker mode="date" format="YYYY-MM-DD" />)}
                 </Form.Item>
-                <div className="clearfix">
+
+                <Form.Item label="태그">
+                  {searchTags.map((tag, index) => {
+                    const isLongTag = tag.length > 6;
+                    const tagElem = (
+                      <Tag
+                        key={index}
+                        color="#5f76f3"
+                        closable
+                        onClose={e => {
+                          e.preventDefault();
+                          handleClose(tag);
+                        }}
+                        style={{ borderRadius: '30px' }}
+                      >
+                        {isLongTag ? `${tag.slice(0, 20)}...` : tag}
+                      </Tag>
+                    );
+
+                    return tagElem;
+                  })}
+                  <AutoComplete
+                    value={input}
+                    dataSource={selectTags === [] ? [] : selectTags}
+                    style={{ width: '100%', padding: '10px 0' }}
+                    onChange={handleTagSearch}
+                    onSelect={onSelect}
+                    notFoundContent={[]}
+                    autoFocus={false}
+                    dropdownRender={
+                      input !== ''
+                        ? menu => (
+                            <div>
+                              {menu}
+                              <Divider style={{ margin: '4px 0' }} />
+                              <div
+                                style={{
+                                  padding: '4px 8px',
+                                  cursor: 'pointer',
+                                }}
+                                onMouseDown={e => e.preventDefault()}
+                                onClick={addItem}
+                              >
+                                <Icon type="plus" /> 새로 추가 : {input}
+                              </div>
+                            </div>
+                          )
+                        : menu => <div></div>
+                    }
+                  >
+                    <Input
+                      prefix={
+                        <Icon
+                          type="search"
+                          style={{ color: 'rgba(0,0,0,.45)' }}
+                        />
+                      }
+                      style={{ width: '100%' }}
+                      size="large"
+                      maxLength={10}
+                      placeholder="예 : 프론트엔드"
+                    />
+                  </AutoComplete>
+                </Form.Item>
+
+                <Form.Item label="이미지 업로드">
                   <Upload
                     action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
                     listType="picture-card"
                     onPreview={handlePreview}
                     onChange={handleChange}
+                    accept="image/*"
                   >
-                    {image === '' ? uploadButton : ''}
+                    {image === null ? uploadButton : null}
                   </Upload>
                   <Modal
                     visible={previewVisible}
@@ -169,7 +298,7 @@ const TeamCreateContainer = props => {
                       src={previewImage}
                     />
                   </Modal>
-                </div>
+                </Form.Item>
                 <Form.Item {...buttonItemLayout}>
                   <Button
                     size="large"
